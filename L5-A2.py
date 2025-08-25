3cc2a9fc51914fa426835dca9fb10651f6d2ffdf
hajwahusham <hajwahusham@gmail.com>
2025-08-25 17:27:46 +0500

    activity02

---
+++ L5-A2.py
@@ -0,0 +1,16 @@
+class person():
+    def __init__(self, fname, lname):
+        self.firstname = fname
+        self.lastname = lname
+
+    def printname(self):
+        print(self.firstname, self.lastname)
+
+class student(person):
+    def __init__(self, fname, lname, year):
+        super().__init__(fname, lname)
+        self.graduationyear = year
+
+x = student("joey", "king", 22021)
+x.printname()
+print(x.graduationyear)
